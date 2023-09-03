9efbe0dee22442186785d16183cf84bbe05a1b49
RunnnnWang <130102197+RunnnnWang@users.noreply.github.com>
2023-09-03 22:06:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 //Exercise I
 //Blinky
+#include "mbed.h"
 DigitalIn button(BUTTON1);
 DigitalOut led(LED1);
 
 int main() {
     while (1) {
-        int x = button;
-        printf("Button is now %d\n", button.read());
-        
         led = button;
-        //led = !led;
         printf("Blink! LED is now %d\n", led.read());
-
         wait_ms(500);
     }
 }
@@ -24,15 +20,18 @@ DigitalOut led(LED1);
 
 int main() {
     while (1) {
+        float number = anInput.read();
         
-        //set the led on for 0.3s
+        printf("Blink! LED is now %.6f Percent\n", number);
+        
+        //the amount of time on
         led = 1;
-        printf("Wow! led is now %d\n", led.read());
-        wait_ms(300);
+        float onTime = number*2000;
+        wait_ms(onTime);
         
-        //set the led equals to anInput, and thus make it off for 1.4s
-        led = anInput;
-        printf("Wow! led is now %d\n", led.read());
-        wait_ms(1400);
+        //the amount of time off
+        led = 0;
+        wait_ms(2000-onTime);
+        
     }
 }
